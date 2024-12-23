f14766b0639ab7cc120d068033f5dab6aca3d045
Karl Villapacibe <kvillapacibe11@gmail.com>
2024-12-23 00:38:16 -0600

    main file

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+/* VERSION NOTES 
+
+	v 1.0:
+		- setup opengl and glfw in vs
+		- create window
+		- set up github pushing/committing
+
+*/
+
+int main()
+{
+	// ----- boilerplate start ----- //
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(
+		800,
+		800,
+		"beamkalc v1.0",
+		NULL, NULL
+	);
+
+	if (window == NULL)
+	{
+		std::cout << "failed to create window." << "\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	glViewport(0, 0, 800, 800);
+	// ----- boilerplate end ----- //
+
+	glClearColor(0.3f, 0.4f, 0.4f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
